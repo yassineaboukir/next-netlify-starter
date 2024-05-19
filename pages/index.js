9156f79b7031447c4785f1f6cc04a230a2575c81
Yassine ABOUKIR <9937267+yassineaboukir@users.noreply.github.com>
2024-05-19 09:27:40 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>PoC</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="PoC by @yassineaboukir" />
       </main>
 
       <Footer />
